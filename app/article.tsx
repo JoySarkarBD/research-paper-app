994dfd5f7e0c2595d156d7b7592099843666f9e9
arunabhnewar <newardevil@gmail.com>
2023-10-05 21:30:29 +0600

    feat: paper page skeleton add

--- app/article.tsx
+++ app/article.tsx
@@ -1,69 +1,86 @@
-import React from 'react';
+import { micromark } from 'micromark';
 import Mermaid from './mermaidtext';
-import {micromark} from 'micromark'
-const BeautifulArticle = ({article_data}) => {
-function parseText(content) {
-  const splitContent = content.split('"""mermaid');
-  
-  const parsedContent = splitContent.map((str, index) => {
-    
-	if (index === 0) return <p key={index}>{str}</p>;
 
-    const diagramAndRest = str.split('"""');
-    const diagram = diagramAndRest[0];
-    const rest = diagramAndRest[1];
+const BeautifulArticle = ({ article_data }) => {
+  function parseText(content) {
+    const splitContent = content.split('"""mermaid');
 
-    // Handle remaining text after diagram
-    let remainingText;
-    if (rest) {
-      const restSplit = rest.split('"""mermaid');
-      remainingText = restSplit.map((subStr, subIndex) => {
-        if (subIndex === 0) return <p key={`${index}_${subIndex}`}>{subStr}</p>;
+    const parsedContent = splitContent.map((str, index) => {
+      if (index === 0) return <p key={index}>{str}</p>;
 
-        const subDiagramAndRest = subStr.split('"""');
-        const subDiagram = subDiagramAndRest[0];
-        const subRest = subDiagramAndRest[1];
-        return (
-          <div key={`${index}_${subIndex}`}>
-            <Mermaid chart={subDiagram.trim()} />
-            {subRest && <p>{subRest}</p>}
-          </div>
-        );
-      });
-    }
+      const diagramAndRest = str.split('"""');
+      const diagram = diagramAndRest[0];
+      const rest = diagramAndRest[1];
 
-    return (
-      <div key={index}>
-        <Mermaid chart={diagram.trim()} />
-        {rest && remainingText}
-      </div>
-    );
-  });
+      // Handle remaining text after diagram
+      let remainingText;
+      if (rest) {
+        const restSplit = rest.split('"""mermaid');
+        remainingText = restSplit.map((subStr, subIndex) => {
+          if (subIndex === 0)
+            return <p key={`${index}_${subIndex}`}>{subStr}</p>;
 
-  return parsedContent;
-}
+          const subDiagramAndRest = subStr.split('"""');
+          const subDiagram = subDiagramAndRest[0];
+          const subRest = subDiagramAndRest[1];
+          return (
+            <div key={`${index}_${subIndex}`}>
+              <Mermaid chart={subDiagram.trim()} />
+              {subRest && <p>{subRest}</p>}
+            </div>
+          );
+        });
+      }
 
+      return (
+        <div key={index}>
+          <Mermaid chart={diagram.trim()} />
+          {rest && remainingText}
+        </div>
+      );
+    });
 
-  console.log("article_data",article_data)
-   
-   const chartDefinition = `
+    return parsedContent;
+  }
+
+  console.log('article_data', article_data);
+
+  const chartDefinition = `
 	`;
-    return (
-	<div className="article format lg:format-lg flex flex-wrap">
-	  <h1>{article_data[3]}</h1>
-	  <div className="w-full lg:w-1/2 p-4">
-		
-		<h2>Problem</h2>
-		<div className="article-content text-gray-700" dangerouslySetInnerHTML={{__html: micromark(article_data[0])}}></div>
-		<h2>New Concepts Introduced and Solution</h2>
-		<div className="article-content text-gray-700" dangerouslySetInnerHTML={{__html: micromark(article_data[1])}}></div>
-	  </div>
-	  <div className="w-full lg:w-1/2 p-4">
-		<h2>Flowcharts explaining the paper</h2>
-		{parseText(article_data[2])}
-	  </div>
-	</div>
-
+  return (
+    <div className=" flex flex-wrap mt-4">
+      <h1 className="text-[#FCFBFE] leading-tight ">{article_data[3]}</h1>
+      <div className="w-full pt-6">
+        <div className="problem_box p-8 rounded-[38px] my-12 ">
+          <h2 className="mt-0 mb-6 text-[#FCFBFE] ">Problem</h2>
+          <div
+            className="article-content text-[#AEB5CA]"
+            id="article-content1"
+            dangerouslySetInnerHTML={{ __html: micromark(article_data[0]) }}
+          ></div>
+        </div>
+        <div className="solution_box p-8 rounded-[38px] my-14 ">
+          <h2 className="mt-0 mb-6 text-[#FCFBFE] ">
+            New Concepts Introduced and Solution
+          </h2>
+          <div
+            className="article-content text-[#AEB5CA]"
+            id="article-content2"
+            dangerouslySetInnerHTML={{ __html: micromark(article_data[1]) }}
+          ></div>
+        </div>
+      </div>
+      <div className="w-full  pt-6">
+        <div className="chart_box p-8 rounded-[38px] mb-14 ">
+          <h2 className="mt-0 mb-6 text-[#FCFBFE] ">
+            Flowcharts explaining the paper
+          </h2>
+          <div className="text-[#AEB5CA]" id="article-content3">
+            {parseText(article_data[2])}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- app/paper/[slug]/page.tsx
+++ app/paper/[slug]/page.tsx
@@ -1,69 +1,55 @@
-"use client";
-import React from "react";
-import { useEffect,useState } from 'react';
-import {
-  Card,
-  Grid,
-  Title,
-  Text
-} from "@tremor/react";
+'use client';
+import { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 import BeautifulArticle from '../../article';
 
 const socket = io('https://premrajnarkhede.online');
 
-export default function BlogPage({params}:{params:{slug:string}}) {
+export default function BlogPage({ params }: { params: { slug: string } }) {
   const [status, setStatus] = useState('');
   const [result, setResult] = useState('');
   const [slug, setSlug] = useState(params);
-  const [artData, setArtData] = useState(["NA","NA",``]);
-  const [regen, setRegen] = useState("0"); 
-  const fetchData = (slug,regen) => {
-  
-	//const fws = document.querySelectorAll('div.mermaid');
-	console.log("slug",slug)
-	socket.on('status', (data) => {
-	  console.log(data)
+  const [artData, setArtData] = useState(['NA', 'NA', ``]);
+  const [regen, setRegen] = useState('0');
+  const fetchData = (slug, regen) => {
+    //const fws = document.querySelectorAll('div.mermaid');
+    console.log('slug', slug);
+    socket.on('status', (data) => {
+      console.log(data);
       setStatus(data);
     });
 
     socket.on('result', (data) => {
-	  console.log("data",data)
-      setArtData([data[0],data[1],""]);
-	  setArtData(data);
+      console.log('data', data);
+      setArtData([data[0], data[1], '']);
+      setArtData(data);
       socket.off('status');
       socket.off('result');
     });
-	socket.emit('search', {"slug":slug,"regen":regen} );
-	
-	
-  }
-  
+    socket.emit('search', { slug: slug, regen: regen });
+  };
+
   useEffect(() => {
     // Ensure the slug exists before making the API call
-       const queryParams = new URLSearchParams(window.location.search);
-	   const regen = queryParams.get("regen") || 0; // fallback to 0 if not found
-	   setRegen(Number(regen)); // Ensure it's a number
-	   console.log("regen",regen)
-      fetchData(slug,regen);
-    
-  },slug);  // This effect runs whenever the slug changes
-  
+    const queryParams = new URLSearchParams(window.location.search);
+    const regen = queryParams.get('regen') || 0; // fallback to 0 if not found
+    setRegen(Number(regen)); // Ensure it's a number
+    console.log('regen', regen);
+    fetchData(slug, regen);
+  }, slug); // This effect runs whenever the slug changes
 
-	
   return (
     <main className="">
-	  
-	
       <div className="">
-		<Card className = "flex flex-col items-center landing-page-card bg-gradient-to-b from-blue-100 via-blue-50 to-white h-screen">
-		  <section className="bg-cover bg-center w-2/3">
-			<p>Status: {status}</p>
-			<BeautifulArticle article_data={artData} />
-			
-		  </section>
-		</Card>
-	  </div>
+        <div className="flex flex-col items-center landing-page-card custom-bg h-auto">
+          <section className="bg-cover bg-center w-2/3">
+            <p className="text-[#FCFBFE] ">
+              Status: <span className="text-[#AEB5CA] ">{status}</span>
+            </p>{' '}
+            <BeautifulArticle article_data={artData} />
+          </section>
+        </div>
+      </div>
     </main>
   );
 }
