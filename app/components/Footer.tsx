a0e9fea20f83634666362038c1c63e5da396a73e
arunabhnewar <newardevil@gmail.com>
2023-10-06 10:49:53 +0600

    fix: footer design fix

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,17 +2,23 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <div className="custom-bg px-4 mx-auto md:px-24 lg:px-8">
-      <div className="flex flex-col-reverse justify-between pt-5 pb-10  lg:flex-row">
-        <p className="text-[15px] text-[#b4bcd0]">
-          © 2023 Copyright PaperPlainer . All Rights Reserved. For
-          feedback/modifications reach out to prem@paperplainer.com
+    <div className="footer-bg px-4 mx-auto md:px-24 lg:px-8">
+      <div className=" pt-14 pb-2 text-center ">
+        <div className="copy-rights"></div>
+        <p className="text-[15px] text-[#b4bcd0] mt-0 leading-none ">
+          © 2023 Copyright <strong className="text-[18px]">PaperPlainer</strong>{' '}
+          . All Rights Reserved.
         </p>
-        <ul className="flex flex-col space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
+        <p className="text-[15px] text-[#b4bcd0] mt-0 leading-none">
+          For feedback/modifications reach out to{' '}
+          <strong className="text-[18px]">prem@paperplainer.com</strong>
+        </p>
+
+        <ul className="">
           <li>
             <Link
               href="/privacy"
-              className="text-[15px] text-[#b4bcd0] transition-colors duration-300 hover:text-deep-purple-accent-400"
+              className="text-[15px] text-[#b4bcd0] transition-colors duration-300 hover:text-white"
             >
               Privacy Policy
             </Link>
