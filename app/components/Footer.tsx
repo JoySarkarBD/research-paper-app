4b72aa8b96eb7acffabc0b568e7f87d66e7e99bc
JoySarkarBD <developer.joysarkar@gmail.com>
2023-10-06 12:46:28 +0600

    footer fix

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,29 +2,27 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <div className="bg-[#020316] px-4 mx-auto md:px-24 lg:px-8">
-      <div className=" pt-14 pb-2 text-center ">
-        <div className="w-[70%] mx-auto my-0 pb-[15px] border-t border-solid border-[rgba(255,255,255,0.18)]"></div>
-        <p className="text-[15px] text-[#b4bcd0] mt-0 leading-none ">
-          © 2023 Copyright <strong className="text-[18px]">PaperPlainer</strong>{' '}
-          . All Rights Reserved.
-        </p>
-        <p className="text-[15px] text-[#b4bcd0] mt-0 leading-none">
-          For feedback/modifications reach out to{' '}
-          <strong className="text-[18px]">prem@paperplainer.com</strong>
-        </p>
+    <footer className="bg-white dark:bg-gray-900">
+      <div className="mx-auto max-w-screen-xl px-4 pb-6 pt-16 sm:px-6 lg:px-8 lg:pt-24">
+        <div className="border-t border-gray-100 pt-6 dark:border-gray-800">
+          <div className="text-center sm:flex sm:items-center sm:justify-between sm:text-left">
+            <b className="text-sm text-gray-500 dark:text-gray-400 mt-0">
+              <Link
+                className=" mt-0 text-sm inline-block dark:text-gray-400 transition hover:dark:text-gray-400/17"
+                href="/privacy"
+              >
+                Privacy Policy
+              </Link>
+            </b>
 
-        <ul className="">
-          <li>
-            <Link
-              href="/privacy"
-              className="text-[15px] text-[#b4bcd0] transition-colors duration-300 hover:text-white"
-            >
-              Privacy Policy
-            </Link>
-          </li>
-        </ul>
+            <p className="mt-4 text-sm text-gray-500 dark:text-gray-400 sm:order-first sm:mt-0">
+              &copy; 2023 Copyright. All Rights Reserved.
+              <br /> For feedback/modifications reach out to{' '}
+              <strong className="text-[18px]">prem@paperplainer.com</strong>
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
+    </footer>
   );
 }
